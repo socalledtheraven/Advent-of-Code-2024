4e913dd3f3ef308d98a7ef78f1b595a26e7a97dc
socalledtheraven <cassie.dalrymple3@gmail.com>
2024-12-05 22:18:43 +0000

    Day 5 part 2 - code's a mess, but it gets the job done note for future that sorted() custom sorts work like this

--- Day 5/Day 5.py
+++ Day 5/Day 5.py
@@ -1,27 +1,24 @@
-def is_before(rules, pair):
-    a, b = pair
-    relevant_rules = [rule for rule in rules if rule.startswith(a)]
+from functools import cmp_to_key
+
+
+def ordering(a, b):
+    # a, b = pair
+    relevant_rules = [rule for rule in rules if rule.startswith(a) or rule.startswith(b)]
 
     for rule in relevant_rules:
         first, second = rule.split("|")
+        # first goes before second
         if first == a and second == b:
-            return True
+            return -1
+        elif first == b and second == a:
+            return 1
 
-    return False
+    return 0
 
 
-def update_in_order(filename):
-    with open(filename, "r") as f:
-        content = f.read()
-
-    rules, updates = content.split("\n\n")
-    rules = [s.strip() for s in rules.splitlines()]
-    updates = [s.strip() for s in updates.splitlines()]
-
-    ordered_updates = []
+def update_in_order():
+    unordered_updates = []
     for update in updates:
-        print(update)
-
         update = update.split(",")
         update_pairs = [update[i:i+2] for i in range(0, len(update), 1)]
         if len(update_pairs[-1]):
@@ -29,18 +26,26 @@ def update_in_order(filename):
 
         ordered = True
         for pair in update_pairs:
-            if not is_before(rules, pair):
+            if ordering(pair[0], pair[1]) > 0:
                 ordered = False
 
-        if ordered:
-            ordered_updates.append(update)
+        if not ordered:
+            unordered_updates.append(update)
 
     total = 0
-    for update in ordered_updates:
-        middle = update[int(len(update)/2)]
+    for upd in unordered_updates:
+        upd = sorted(upd, key=cmp_to_key(ordering))
+        middle = upd[int(len(upd)/2)]
         total += int(middle)
 
     print(f"total: {total}")
 
 
-update_in_order("Day 5 Input")
+filename = "Day 5 Input"
+with open(filename, "r") as f:
+    content = f.read()
+
+rules, updates = content.split("\n\n")
+rules = [s.strip() for s in rules.splitlines()]
+updates = [s.strip() for s in updates.splitlines()]
+update_in_order()
